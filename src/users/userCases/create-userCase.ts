b14d5565a1ccd39968b7239489aa02a0c96bdd31
joaocastro <joaocastro0429@gmail.com>
2025-04-09 17:39:01 -0300

    add o bcrypt

--- src/users/userCases/create-userCase.ts
+++ src/users/userCases/create-userCase.ts
@@ -1,6 +1,7 @@
 import { PrismaService } from "src/infra/prisma/prisma.service"
 import { CreateUserDTO } from "../dto/user.dto"
 import { Injectable } from "@nestjs/common"
+import {hash} from 'bcrypt'
 
 
 
@@ -13,16 +14,22 @@ export class createUserUserCase{
         where: {
           OR: [{ username: data.username }, { email: data.email }],
         },
+
       })
       
       if (user) {
         throw new Error("User already exists")
       }
+      const passwordHash = await hash(data.password,10)
+
 
         return await this.prisma.users.create({
-            data,
+            data:{
+                ...data,
+                password: passwordHash,
+            },
         })
-
+    
 
     }
 }
