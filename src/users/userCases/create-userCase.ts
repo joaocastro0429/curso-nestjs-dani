a3c897467d68e086faf4bc1f27f27c5630763ea6
joaocastro <joaocastro0429@gmail.com>
2025-04-08 22:03:44 -0300

    criando o banco com o prisma

---
+++ src/users/userCases/create-userCase.ts
@@ -0,0 +1,31 @@
+import { PrismaService } from "src/infra/prisma/prisma.service"
+
+type CreateUserDTO={
+    username:string 
+    email:string
+    password:string
+    name:string
+
+}
+
+
+export class createUserUserCase{
+    constructor(private prisma:PrismaService){}
+   async execute(data:CreateUserDTO){
+       const user= await this.prisma.users.findFirst({
+            where:{
+                OR:[{username:data.username},{email:data.email}],
+            }
+        })
+
+        if(user){
+            throw new  Error("User already exists")
+        }
+
+        return await this.prisma.users.create({
+            data,
+        })
+
+
+    }
+}
